b78b7b93c841f5fa6264703cab1914bbcb56c479
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-07-22 22:21:49 +0200

    Added Scaladoc comments.

--- core/src/main/scala/it/parttimeteam/Suit.scala
+++ core/src/main/scala/it/parttimeteam/Suit.scala
@@ -1,19 +1,48 @@
 package it.parttimeteam
 
+/**
+ * One of the 4 suit of the deck.
+ *
+ * @param name      Name.
+ * @param shortName Symbol.
+ */
 abstract class Suit(val name: String, val shortName: String) extends Serializable
 
 object Suit {
 
+  /**
+   * The Club suit.
+   */
   case class Clubs() extends Suit("Clubs", "♣")
 
+  /**
+   * The spade suit.
+   */
   case class Spades() extends Suit("Spades", "♠")
 
+  /**
+   * The diamond suit.
+   */
   case class Diamonds() extends Suit("Diamonds", "♦")
 
+  /**
+   * The heart suit.
+   */
   case class Hearts() extends Suit("Hearts", "♥")
 
+  /**
+   * Get all suits of the deck.
+   *
+   * @return A list with all suits.
+   */
   def all: List[Suit] = List(Clubs(), Spades(), Diamonds(), Hearts())
 
+  /**
+   * Transform a string in the matching suit.
+   *
+   * @param s String to convert.
+   * @return Suit converted.
+   */
   implicit def string2suit(s: String): Suit = s match {
     case "♣" => Clubs()
     case "♠" => Spades()
