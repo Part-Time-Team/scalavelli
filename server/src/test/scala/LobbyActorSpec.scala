accd51038d9ac908d13c5ea39bc6b24404da4b04
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-07-23 23:15:20 +0200

    defined main lobby test

--- server/src/test/scala/LobbyActorSpec.scala
+++ server/src/test/scala/LobbyActorSpec.scala
@@ -1,12 +1,15 @@
-import Messages.{ConnectUser, UserConnectionAccepted}
+import Messages.{ConnectUser, MatchFound, UserConnectionAccepted}
 import akka.actor.ActorSystem
-import akka.testkit.{ImplicitSender, TestKit}
-import org.scalatest.funspec.AnyFunSpec
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import org.scalatest.BeforeAndAfterEach
 import org.scalatest.wordspec.AnyWordSpecLike
 
 class LobbyActorSpec extends TestKit(ActorSystem())
   with ImplicitSender
-  with AnyWordSpecLike {
+  with AnyWordSpecLike
+  with BeforeAndAfterEach {
+
+  override protected def afterEach(): Unit = TestKit.shutdownActorSystem(system)
 
   "a lobby actor" should {
 
@@ -16,6 +19,30 @@ class LobbyActorSpec extends TestKit(ActorSystem())
       expectMsg(UserConnectionAccepted)
     }
 
+
+    "reply with a match found message on players number reached" in {
+      val lobbyActor = system.actorOf(LobbyActor.props())
+      val client1 = TestProbe("client1")
+      val client2 = TestProbe("client2")
+      val client3 = TestProbe("client3")
+      val client4 = TestProbe("client4")
+
+      client1.send(lobbyActor, ConnectUser("client1", 4))
+      client2.send(lobbyActor, ConnectUser("client2", 4))
+      client3.send(lobbyActor, ConnectUser("client3", 4))
+      client4.send(lobbyActor, ConnectUser("client4", 4))
+
+      client1.expectMsg(UserConnectionAccepted)
+      client2.expectMsg(UserConnectionAccepted)
+      client3.expectMsg(UserConnectionAccepted)
+      client4.expectMsg(UserConnectionAccepted)
+
+      client1.expectMsgClass(classOf[MatchFound])
+      client2.expectMsgClass(classOf[MatchFound])
+      client3.expectMsgClass(classOf[MatchFound])
+      client4.expectMsgClass(classOf[MatchFound])
+
+    }
 
   }
 
