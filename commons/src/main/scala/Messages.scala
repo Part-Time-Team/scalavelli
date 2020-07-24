98b3fec5f8343a83c81aa4c6488fc9979d92012e
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-07-24 16:01:09 +0200

    UserConnectionRefused message class

--- commons/src/main/scala/Messages.scala
+++ commons/src/main/scala/Messages.scala
@@ -10,6 +10,7 @@ object Messages {
 
   // server responses
   case object UserConnectionAccepted
+  case class UserConnectionRefused(reason: String)
   case class MatchFound(gameRoom: ActorRef)
   case class PrivateLobbyCreated(lobbyCode: String)
 
