049990530690857c0128baab55c95bb70f0e09c2
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-07-25 12:26:01 +0200

    Finish property testing on suit

--- core/src/test/scala/it/parttimeteam/SuitSuite.scala
+++ core/src/test/scala/it/parttimeteam/SuitSuite.scala
@@ -4,7 +4,7 @@ import it.parttimeteam.Suit.{Clubs, Diamonds, Hearts, Spades}
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should
-import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.prop.{TableDrivenPropertyChecks, TableFor1}
 import org.scalatest.propspec.AnyPropSpec
 
 class SuitSuite extends AnyFunSuite {
@@ -27,9 +27,13 @@ class SuitSuite extends AnyFunSuite {
   }
 }
 
-class SuitPropSpec extends AnyPropSpec with TableDrivenPropertyChecks with should.Matchers {
+class SuitPropSpec
+  extends AnyPropSpec
+    with TableDrivenPropertyChecks
+    with should.Matchers {
 
-  val examples = Table(
+  // Property test for all suits.
+  val examples: TableFor1[Suit] = Table(
     "suit",
     Clubs(),
     Spades(),
@@ -37,31 +41,32 @@ class SuitPropSpec extends AnyPropSpec with TableDrivenPropertyChecks with shoul
     Hearts()
   )
 
+  val failures: TableFor1[String] = Table(
+    "suit",
+    "s",
+    "A",
+    "J",
+    "Q",
+    "K"
+  )
+
   property("string2suit must return shortName at name input") {
     forAll(examples) { suit =>
-      Suit.string2suit(suit.shortName).name should be (suit.name)
+      (Suit string2suit suit.shortName).name should be(suit.name)
+    }
+  }
+
+  property("Invoking string2suit with other strings must return RuntimeException") {
+    forAll(failures) { fail =>
+      a[RuntimeException] should be thrownBy {
+        Suit string2suit fail
+      }
     }
   }
 }
 
 class SuitSpec extends AnyFunSpec {
   describe("Suit conversions") {
-    it("Should be Clubs") {
-      assert(Suit string2suit "♣" equals Clubs())
-    }
-
-    it("Should be Spades") {
-      assert(Suit string2suit "♠" equals Spades())
-    }
-
-    it("Should be Diamonds") {
-      assert(Suit string2suit "♦" equals Diamonds())
-    }
-
-    it("Should be Hearts") {
-      assert(Suit string2suit "♥" equals Hearts())
-    }
-
     it("Should raise RuntimeException for s string") {
       assertThrows[RuntimeException] {
         Suit.string2suit("s")
