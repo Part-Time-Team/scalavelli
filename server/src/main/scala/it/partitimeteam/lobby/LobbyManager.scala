6f45b6c3f250250a651f0a306d8ec704e1dab66d
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-07-27 18:16:33 +0200

    refactor LobbyManager trait

--- server/src/main/scala/it/partitimeteam/lobby/LobbyManager.scala
+++ server/src/main/scala/it/partitimeteam/lobby/LobbyManager.scala
@@ -2,28 +2,55 @@ package it.partitimeteam.lobby
 
 import it.partitimeteam.common.Player
 
+object LobbyManager {
+  def apply(): LobbyManager[Player] = new LobbyManagerImpl[Player]()
+}
+
+
 trait LobbyManager[T <: Player] {
 
   /**
+   * Add a player to the lobby system
    *
+   * @param player    player to be added
    * @param lobbyType type of the lobby
-   * @param
-   * @return
+   * @return a new lobby manger with the updated lobby
    */
   def addPlayer(player: Player, lobbyType: LobbyType): LobbyManager[T]
 
   /**
+   * Remove a player from the lobby system
    *
-   *
-   * @return
+   * @param playerId the id of the player to remove
+   * @return a new lobbymanger without the specified player
    */
   def removePlayer(playerId: String): LobbyManager[T]
 
   /**
    *
-   * @param lobbyType
+   * @param lobbyType type of the lobby to retrieve
    * @return the lobbing corresponding the the specified type, if present
    */
   def getLobby(lobbyType: LobbyType): Option[Lobby[T]]
 
 }
+
+
+case class LobbyManagerImpl[T <: Player]() extends LobbyManager[T] {
+
+  /**
+   * @inheritdoc
+   */
+  override def addPlayer(player: Player, lobbyType: LobbyType): LobbyManager[T] = ???
+
+  /**
+   * @inheritdoc
+   */
+  override def removePlayer(playerId: String): LobbyManager[T] = ???
+
+  /**
+   * @inheritdoc
+   */
+  override def getLobby(lobbyType: LobbyType): Option[Lobby[T]] = ???
+}
+
