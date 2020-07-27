12b3f2c3ec056e5e217d4fc356484d00c23665e6
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-07-27 07:31:20 +0100

    Create entity Hand

--- core/src/main/scala/it/parttimeteam/Player.scala
+++ core/src/main/scala/it/parttimeteam/Player.scala
@@ -9,10 +9,24 @@ sealed trait Player{
    * Name of the player
    */
   val name:String
+
+  /**
+   * Id of the player
+   */
+  val id:String
+
+  /**
+   * Hand of the player
+   */
+  val hand: Int // Hand
 }
 
 /**
  * Player class
  * @param name the name of the player
+ * @param id the id of the player
+ * @param hand the hand of the player
  */
-case class ScalavelliPlayer(override val name:String) extends Player
+case class ScalavelliPlayer(override val name:String,
+                            override val id: String,
+                            override val hand: Int) extends Player
