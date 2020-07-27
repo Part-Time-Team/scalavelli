703f0e21f978172b18ce51175945ebb9152a4945
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-07-27 12:29:07 +0200

    Create sort method and test

--- core/src/main/scala/it/parttimeteam/Hand.scala
+++ core/src/main/scala/it/parttimeteam/Hand.scala
@@ -1,51 +1,71 @@
 package it.parttimeteam
 
 /**
+ * Player's hand
  *
- * @param playerCards
- * @param tableCards
+ * @param playerCards list of cards in the player's hand
+ * @param tableCards list of cards in the player's hand to put at the table
  */
 case class Hand(playerCards: List[Card] = List(), tableCards: List[Card] = List()) {
 
   /**
-   * Add card to hand player
-   * @param card card to added
-   * @return new case class Hand with update specific list
+   * Add card to the list playerCards
+   *
+   * @param card card to add
+   * @return new Hand with the updated playerCards list
    */
   def addPlayerCard(card: Card): Hand = {
     Hand(playerCards :+ card, tableCards)
   }
 
   /**
-   * Add cards to hand player
-   * @param cards cards to added
-   * @return new case class Hand with update specific list
+   * Add cards to the list playerCards
+   *
+   * @param cards cards to add
+   * @return new Hand with the updated playerCards list
    */
   def addPlayerCards(cards: Card*): Hand = {
     Hand(playerCards ++ cards.toList, tableCards)
   }
 
   /**
-   * Add card to table player
-   * @param card card to added
-   * @return new case class Hand with update specific list
+   * Add card to the list tableCards
+   *
+   * @param card card to add
+   * @return new Hand the updated tableCards list
    */
   def addTableCard(card :Card):Hand = {
     Hand(playerCards, tableCards :+ card)
   }
 
   /**
-   * Add cards to table player
-   * @param cards cards to added
-   * @return new case class Hand with update specific list
+   * Add cards to the list tablePlayer
+   *
+   * @param cards cards to add
+   * @return new Hand the updated tableCards list
    */
   def addTableCards(cards :Card*):Hand = {
     Hand(playerCards, tableCards ++ cards.toList)
   }
 
 
-  /*
-  def orderBySuit()
-  def orderByValue()
-  */
+  /**
+   * Sort cards by value
+   *
+   * @param cards cards to order
+   * @return sorted list of cards
+   */
+  def sortByValue(cards : Card*): List[Card] = {
+    cards.sortWith((s: Card, t: Card) => s.rank.value < t.rank.value).toList
+  }
+
+  /**
+   * Sort cards by suit
+   *
+   * @param cards cards to order
+   * @return sorted list of cards
+   */
+  def sortBySuit(cards: Card*): List[Card] = {
+    cards.sortWith((s:Card, t:Card) => s.suit.order < t.suit.order).toList
+  }
 }
--- core/src/test/scala/it/parttimeteam/HandSpec.scala
+++ core/src/test/scala/it/parttimeteam/HandSpec.scala
@@ -4,19 +4,19 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class HandSpec extends AnyFunSuite{
 
-  test("Check add a one card to list playerCard"){
+  test("Check add a card to the list playerCard"){
     val hand: Hand = Hand()
     val card: Card = Card(Rank.Ace(), Suit.Clubs())
     assert(hand.addPlayerCard(card) equals Hand(List(card)))
   }
 
-  test("Check add a one card to list tableCard"){
+  test("Check add a card to the list tableCard"){
     val hand: Hand = Hand()
     val card: Card = Card(Rank.Ace(), Suit.Clubs())
     assert(hand.addTableCard(card) equals Hand(List(), List(card)))
   }
 
-  test("Check add a more card to list playerCard"){
+  test("Check add more cards to the list playerCard"){
     val card1: Card = Card(Rank.Ace(), Suit.Clubs())
     val card2: Card = Card(Rank.Four(), Suit.Spades())
     val card3: Card = Card(Rank.King(), Suit.Diamonds())
@@ -25,7 +25,7 @@ class HandSpec extends AnyFunSuite{
     assert(hand.addPlayerCards(card2, card3) equals Hand(List(card1, card2, card3)))
   }
 
-  test("Check add a more card to list tableCard"){
+  test("Check add more cards to the list tableCard"){
     val card1: Card = Card(Rank.Ace(), Suit.Clubs())
     val card2: Card = Card(Rank.Four(), Suit.Spades())
     val card3: Card = Card(Rank.King(), Suit.Diamonds())
@@ -33,5 +33,25 @@ class HandSpec extends AnyFunSuite{
 
     val hand: Hand = Hand(List(card4), List(card1))
     assert(hand.addTableCards(card3, card2) equals Hand(List(card4), List(card1, card3, card2)))
+  }
+
+  test("Sort cards by value"){
+    val card1: Card = Card(Rank.Ace(), Suit.Clubs())
+    val card2: Card = Card(Rank.Four(), Suit.Spades())
+    val card3: Card = Card(Rank.King(), Suit.Diamonds())
+    val card4: Card = Card(Rank.Queen(), Suit.Diamonds())
+
+    val hand : Hand = Hand(List(), List())
+    assert(hand.sortByValue(card3, card4, card1, card2) equals List(card1, card2, card4, card3))
+  }
+
+  test("Sort cards by suit"){
+    val card1: Card = Card(Rank.Two(), Suit.Clubs())
+    val card2: Card = Card(Rank.Four(), Suit.Spades())
+    val card3: Card = Card(Rank.King(), Suit.Hearts())
+    val card4: Card = Card(Rank.Queen(), Suit.Diamonds())
+
+    val hand : Hand = Hand(List(), List())
+    assert(hand.sortBySuit(card1, card2, card3, card4) equals List(card3, card4, card1, card2))
   }
 }
