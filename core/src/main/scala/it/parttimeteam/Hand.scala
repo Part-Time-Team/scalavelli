9ef68b0dc53be4d6a94d7c396d1f2199c84baeec
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-07-27 07:35:09 +0100

    Create Test entity Hand

--- core/src/main/scala/it/parttimeteam/Hand.scala
+++ core/src/main/scala/it/parttimeteam/Hand.scala
@@ -1,5 +1,51 @@
 package it.parttimeteam
 
-class Hand {
+/**
+ *
+ * @param playerCards
+ * @param tableCards
+ */
+case class Hand(playerCards: List[Card] = List(), tableCards: List[Card] = List()) {
 
+  /**
+   * Add card to hand player
+   * @param card card to added
+   * @return new case class Hand with update specific list
+   */
+  def addPlayerCard(card: Card): Hand = {
+    Hand(playerCards :+ card, tableCards)
+  }
+
+  /**
+   * Add cards to hand player
+   * @param cards cards to added
+   * @return new case class Hand with update specific list
+   */
+  def addPlayerCards(cards: Card*): Hand = {
+    Hand(playerCards ++ cards.toList, tableCards)
+  }
+
+  /**
+   * Add card to table player
+   * @param card card to added
+   * @return new case class Hand with update specific list
+   */
+  def addTableCard(card :Card):Hand = {
+    Hand(playerCards, tableCards :+ card)
+  }
+
+  /**
+   * Add cards to table player
+   * @param cards cards to added
+   * @return new case class Hand with update specific list
+   */
+  def addTableCards(cards :Card*):Hand = {
+    Hand(playerCards, tableCards ++ cards.toList)
+  }
+
+
+  /*
+  def orderBySuit()
+  def orderByValue()
+  */
 }
--- core/src/test/scala/it/parttimeteam/HandSpec.scala
+++ core/src/test/scala/it/parttimeteam/HandSpec.scala
@@ -1,5 +1,37 @@
 package it.parttimeteam
 
-object HandSuite {
+import org.scalatest.funsuite.AnyFunSuite
 
+class HandSpec extends AnyFunSuite{
+
+  test("Check add a one card to list playerCard"){
+    val hand: Hand = Hand()
+    val card: Card = Card(Rank.Ace(), Suit.Clubs())
+    assert(hand.addPlayerCard(card) equals Hand(List(card)))
+  }
+
+  test("Check add a one card to list tableCard"){
+    val hand: Hand = Hand()
+    val card: Card = Card(Rank.Ace(), Suit.Clubs())
+    assert(hand.addTableCard(card) equals Hand(List(), List(card)))
+  }
+
+  test("Check add a more card to list playerCard"){
+    val card1: Card = Card(Rank.Ace(), Suit.Clubs())
+    val card2: Card = Card(Rank.Four(), Suit.Spades())
+    val card3: Card = Card(Rank.King(), Suit.Diamonds())
+
+    val hand: Hand = Hand(List(card1))
+    assert(hand.addPlayerCards(card2, card3) equals Hand(List(card1, card2, card3)))
+  }
+
+  test("Check add a more card to list tableCard"){
+    val card1: Card = Card(Rank.Ace(), Suit.Clubs())
+    val card2: Card = Card(Rank.Four(), Suit.Spades())
+    val card3: Card = Card(Rank.King(), Suit.Diamonds())
+    val card4: Card = Card(Rank.Queen(), Suit.Diamonds())
+
+    val hand: Hand = Hand(List(card4), List(card1))
+    assert(hand.addTableCards(card3, card2) equals Hand(List(card4), List(card1, card3, card2)))
+  }
 }
