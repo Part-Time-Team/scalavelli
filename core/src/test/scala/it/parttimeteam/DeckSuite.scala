745cd2edf9a28cc59830438e0675cba597c9bfb7
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-07-28 19:20:29 +0200

    Complete testing deck entity

--- core/src/test/scala/it/parttimeteam/DeckSuite.scala
+++ core/src/test/scala/it/parttimeteam/DeckSuite.scala
@@ -8,9 +8,9 @@ class DeckSuite extends AnyFunSuite {
     val deck: Deck = Deck.sorted
     val before = deck.remaining
     assert(before equals 52)
-    val card = deck.draw()
-    assert(!(card.name() equals ""))
-    assert(deck.remaining equals 51)
+    val cardDrawn = deck.draw()
+    assert(cardDrawn._1.remaining equals 51)
+    assert(!(cardDrawn._2.name() equals ""))
   }
 
   /*test("Draw the first card of the deck") {
@@ -35,10 +35,13 @@ class DeckSpec extends AnyFunSpec {
     }
 
     it("should be empty after some draws") {
-      val deckToEmpty = Deck.shuffled
+      var deckToEmpty = Deck.shuffled
       assert(!deckToEmpty.isEmpty)
       // For each card, draw a card.
-      for {_ <- Deck.sorted.cards} deckToEmpty.draw()
+      for {_ <- Deck.sorted.cards} {
+        val cardDrawn = deckToEmpty.draw()
+        deckToEmpty = cardDrawn._1
+      }
       assert(deckToEmpty.isEmpty)
     }
   }
