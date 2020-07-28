fb4c77ba3d7a37c407c13d9c9c4ab5f4cb343b37
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-07-28 22:06:51 +0200

    LobbyMangerActor partial implementation - missing private lobby management

--- server/src/main/scala/it/partitimeteam/lobby/LobbyManagerActor.scala
+++ server/src/main/scala/it/partitimeteam/lobby/LobbyManagerActor.scala
@@ -1,7 +1,10 @@
 package it.partitimeteam.lobby
 
+import java.util.UUID
+
 import akka.actor.{Actor, Props}
-import it.parttimeteam.messages.Messages.{ConnectUserToPublicLobby, UserConnectionAccepted}
+import it.partitimeteam.common.GamePlayer
+import it.parttimeteam.messages.Messages.{Connect, ConnectUserToPrivateLobby, ConnectUserToPublicLobby, LeaveLobby, LobbyConnectionAccepted, PrivateLobbyCreated, RequestPrivateLobbyCreation, UserConnectionAccepted, UserConnectionRefused}
 
 object LobbyManagerActor {
 
@@ -11,10 +14,45 @@ object LobbyManagerActor {
 
 class LobbyManagerActor extends Actor {
 
+  type UserName = String
+  type UserId = String
+
+  private var connectedPlayers: Map[UserId, UserName] = Map()
+  private val lobbyManger = new LobbyManagerImpl[GamePlayer]()
+
+
   override def receive: Receive = {
-    case ConnectUserToPublicLobby(userName, numberOfPlayers) => {
-      sender() ! UserConnectionAccepted
+    case Connect(userName) => {
+      val userId = UUID.randomUUID().toString
+      connectedPlayers = connectedPlayers + (userId -> userName)
+      sender() ! UserConnectionAccepted(userId)
     }
+    case ConnectUserToPublicLobby(userId, numberOfPlayers) => {
+      connectedPlayers.get(userId) match {
+        case Some(username) => {
+          this.lobbyManger.addPlayer(GamePlayer(userId, username, sender()), PlayerNumberLobby(numberOfPlayers))
+          sender() ! LobbyConnectionAccepted
+        }
+        case None => sender() ! UserConnectionRefused("Invalid user id")
+      }
+
+    }
+    case RequestPrivateLobbyCreation(userId, numberOfPlayers) => {
+      val lobbyId = UUID.randomUUID().toString
+      connectedPlayers.get(userId) match {
+        case Some(username) => {
+          this.lobbyManger.addPlayer(GamePlayer(userId, username, sender()), PrivateLobby(lobbyId, numberOfPlayers))
+          sender() ! PrivateLobbyCreated(lobbyId)
+
+        }
+        case None => 
+      }
+
+    }
+    case ConnectUserToPrivateLobby(userId, lobbyCode) =>
+    case LeaveLobby(userId) => this.lobbyManger.removePlayer(userId)
+
   }
+
 
 }
