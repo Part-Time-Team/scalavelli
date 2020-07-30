a0ebb097b419e77cc14ca96ea0ae42a3816d366d
Luca Giorgetti <lucagiorgit95@gmail.com>
2020-07-30 18:32:13 +0200

    Add first scene graphics

--- client/src/main/scala/it/parttimeteam/view/startup/scenes/SelectScene.scala
+++ client/src/main/scala/it/parttimeteam/view/startup/scenes/SelectScene.scala
@@ -1,19 +1,32 @@
 package it.parttimeteam.view.startup.scenes
 
-import it.parttimeteam.view.utils.ButtonFactory
-import it.parttimeteam.view.BaseScene
 import it.parttimeteam.view.startup.listeners.SelectSceneListener
+import it.parttimeteam.view.utils.ButtonFactory
 import scalafx.geometry.Pos.Center
+import scalafx.scene.Scene
 import scalafx.scene.control.Button
+import scalafx.scene.image.{Image, ImageView}
 import scalafx.scene.layout.{BorderPane, VBox}
 import scalafx.stage.Stage
 
-class SelectScene(override val parentStage: Stage, val listener: SelectSceneListener) extends BaseScene(parentStage) {
+class SelectScene(val parentStage: Stage, val listener: SelectSceneListener) extends Scene() {
+  val background: ImageView = new ImageView(new Image("/images/background.png")) {
+    fitWidth <== parentStage.width
+    fitHeight <== parentStage.height
+  }
+
+  val title: ImageView = new ImageView(new Image("/images/game_title.png")) {
+    fitWidth <== parentStage.width / 3
+    preserveRatio = true
+  }
+
   val btnPlayersNum: Button = ButtonFactory.makeButton("Start new game", listener.onSelectedGameWithPlayers, 200d)
   val btnCode: Button = ButtonFactory.makeButton("Participate with a code", listener.onSelectedGameWithCode, 200d)
   val btnNewCode: Button = ButtonFactory.makeButton("Create new code", listener.onSelectedCreatePrivateGame, 200d)
 
   val borderPane: BorderPane = new BorderPane()
+  borderPane.prefWidth <== parentStage.width
+  borderPane.prefHeight <== parentStage.height
 
   val center: VBox = new VBox()
   center.alignment = Center
@@ -21,7 +34,7 @@ class SelectScene(override val parentStage: Stage, val listener: SelectSceneList
 
   borderPane.center = center
 
-  center.getChildren.addAll(btnPlayersNum, btnCode, btnNewCode)
+  center.getChildren.addAll(title, btnPlayersNum, btnCode, btnNewCode)
 
-  root = borderPane
+  content = Seq(background, borderPane)
 }
