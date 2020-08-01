1f34a7604c6648f1e39845a9dc4a54cf278d5f5b
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-08-01 12:06:31 +0200

    test to pass to simulate a game match initialization: from lobby to game

---
+++ server/src/test/scala/it/parttimeteam/LobbyAndMatchStartFlowSpec.scala
@@ -0,0 +1,46 @@
+package it.parttimeteam
+
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import it.partitimeteam.lobby.LobbyManagerActor
+import it.parttimeteam.entities.GamePlayerState
+import it.parttimeteam.messages.LobbyMessages.{JoinPublicLobby, UserAddedToLobby}
+import it.parttimeteam.messages.GameMessage.{GameStarted, Ready}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class LobbyAndMatchStartFlowSpec extends TestKit(ActorSystem())
+  with ImplicitSender
+  with AnyWordSpecLike
+  with BeforeAndAfterAll {
+
+  private val NUMBER_OF_PLAYERS = 2
+
+  override protected def afterAll(): Unit = TestKit.shutdownActorSystem(system)
+
+  "A correct flow" should {
+
+    val lobbyActor = system.actorOf(LobbyManagerActor.props()) // TestActorRef[LobbyManagerActor](LobbyManagerActor.props())
+    val probe = TestProbe()
+
+    "add users to lobby and if number of player is enough, start a match and notify the players" in {
+      probe.send(lobbyActor, JoinPublicLobby("otherPlayer", NUMBER_OF_PLAYERS))
+      lobbyActor ! JoinPublicLobby("me", NUMBER_OF_PLAYERS)
+
+      probe.expectMsgType[UserAddedToLobby]
+      expectMsgType[UserAddedToLobby]
+
+      probe.expectMsg(GameStarted)
+      probe.send(lobbyActor, Ready)
+      expectMsg(GameStarted)
+      lobbyActor ! Ready
+
+      probe.expectMsgType[GamePlayerState]
+      expectMsgType[GamePlayerState]
+
+    }
+
+  }
+
+
+}
