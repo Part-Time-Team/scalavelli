9a6f3a1cf00b712ec838fdeb9e70f3cc54362e74
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-08-08 14:37:01 +0200

    common constants object

--- server/src/main/scala/it/partitimeteam/ScalavelliServer.scala
+++ server/src/main/scala/it/partitimeteam/ScalavelliServer.scala
@@ -2,12 +2,13 @@ package it.partitimeteam
 
 import akka.actor.ActorSystem
 import it.partitimeteam.lobby.LobbyManagerActor
+import it.parttimeteam.Constants
 
 object ScalavelliServer {
   def main(args: Array[String]): Unit = {
 
-    val system = ActorSystem("ScalavelliServer")
-    system.actorOf(LobbyManagerActor.props(), "lobby")
+    val system = ActorSystem(Constants.Remote.SERVER_ACTOR_SYSTEM_NAME)
+    system.actorOf(LobbyManagerActor.props(), Constants.Remote.SERVER_LOBBY_ACTOR_NAME)
 
   }
 }
