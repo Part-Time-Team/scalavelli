ac84f66035346dff80809dec6ef80022cf9e9dc8
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-08-17 11:54:55 +0200

    Init test to prologGameConverter

--- core/src/test/scala/it/parttimeteam/Prolog/PrologGameConverterSpec.scala
+++ core/src/test/scala/it/parttimeteam/Prolog/PrologGameConverterSpec.scala
@@ -1,5 +1,24 @@
 package it.parttimeteam.Prolog
 
-class PrologGameConverter {
+import alice.tuprolog.{Term, Var}
+import it.parttimeteam.Prolog.converter.PrologGameConverter
+import it.parttimeteam.Prolog.engine.{PrologGameEngine, PrologStruct}
+import org.scalatest.funsuite.AnyFunSuite
 
+class PrologGameConverterSpec extends AnyFunSuite{
+
+  test("Convert term to int"){
+
+    val engine : PrologGameEngine = new PrologGameEngine
+    val gameConverter: PrologGameConverter = new PrologGameConverter
+
+    val numberCard :List[Term] = engine goal PrologStruct("startHand", new Var("X"))
+    assert(gameConverter.toInt(numberCard.head) equals 13)
+  }
+
+  test("Convert term to boolean"){}
+
+  test("Convert term to string"){}
+
+  test("Convert list of cards to string"){}
 }
