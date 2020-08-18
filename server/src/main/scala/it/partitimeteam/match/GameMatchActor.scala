bc24534ffa3717cbf16560ba17b14f13d6d915e5
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-08-18 16:03:00 +0200

    players as class variable

--- server/src/main/scala/it/partitimeteam/match/GameMatchActor.scala
+++ server/src/main/scala/it/partitimeteam/match/GameMatchActor.scala
@@ -1,12 +1,13 @@
 package it.partitimeteam.`match`
 
 import akka.actor.{Actor, ActorLogging, Props, Stash}
-import it.parttimeteam.Board
 import it.parttimeteam.core.collections.Hand
+import it.parttimeteam.core.{GameManager, GameManagerImpl}
 import it.parttimeteam.entities.GamePlayer
 import it.parttimeteam.gamestate.PlayerGameState
 import it.parttimeteam.messages.GameMessage.{GamePlayers, Ready}
 import it.parttimeteam.messages.LobbyMessages.MatchFound
+import it.parttimeteam.{Board, GameState}
 
 object GameMatchActor {
   def props(numberOfPlayers: Int): Props = Props(new GameMatchActor(numberOfPlayers))
@@ -16,27 +17,36 @@ class GameMatchActor(numberOfPlayers: Int) extends Actor with ActorLogging with 
 
   override def receive: Receive = idle
 
+  private val gameManager: GameManager = new GameManagerImpl()
+  private var players: Seq[GamePlayer] = _
+
   private def idle: Receive = {
     case GamePlayers(players) => {
+      this.players = players
       require(players.size == numberOfPlayers)
-      this.broadcastMessageToPlayers(players)(MatchFound(self))
-      context.become(initializing(players, Seq.empty))
+      this.broadcastMessageToPlayers(MatchFound(self))
+      context.become(initializing(Seq.empty))
       unstashAll()
     }
     case _ => stash()
   }
 
-  private def initializing(players: Seq[GamePlayer], playersReady: Seq[GamePlayer]): Receive = {
+  /**
+   * Waits all players to start game
+   *
+   * @param playersReady players ready for the game
+   */
+  private def initializing(playersReady: Seq[GamePlayer]): Receive = {
     case Ready(id) => {
-      players.find(p => p.id == id) match {
+      this.players.find(_.id == id) match {
         case Some(p) => {
           val updatedReadyPlayers = playersReady :+ p
           if (updatedReadyPlayers.length == numberOfPlayers) {
             log.debug("All players ready")
-            this.broadcastMessageToPlayers(players)(PlayerGameState(Board(), Hand(), Seq.empty))
-            context.become(inGame())
+            this.initializeGame()
+
           } else {
-            context.become(initializing(players, updatedReadyPlayers))
+            context.become(initializing(updatedReadyPlayers))
           }
         }
         case None => log.debug(s"Player id $id not found")
@@ -44,12 +54,32 @@ class GameMatchActor(numberOfPlayers: Int) extends Actor with ActorLogging with 
     }
   }
 
-  private def inGame(): Receive = {
+  /**
+   * Inizialize the game, creating the initial state, the turn manager and changing actor behaviour
+   *
+   */
+  private def initializeGame(): Unit = {
+    val gameState = gameManager.create(players.map(_.id))
+    context.become(inGame(gameState, TurnManager(players.map(_.id))))
+    this.broadcastGameStateToPlayers(gameState)
+  }
+
+  private def inGame(gameState: GameState, turnManager: TurnManager): Receive = {
     case _ =>
   }
 
-  private def broadcastMessageToPlayers(players: Seq[GamePlayer])(message: Any): Unit = {
-    players.foreach(p => p.actorRef ! message)
+
+  private def broadcastMessageToPlayers(message: Any): Unit = {
+    this.players.foreach(p => p.actorRef ! message)
+  }
+
+  /**
+   * Send and update message about the game state to each player
+   *
+   * @param gameState the global game state
+   */
+  private def broadcastGameStateToPlayers(gameState: GameState) {
+    this.broadcastMessageToPlayers(PlayerGameState(Board(), Hand(), Seq.empty))
   }
 
 }
