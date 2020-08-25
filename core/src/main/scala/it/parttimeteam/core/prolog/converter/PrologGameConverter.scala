cc57cd06dcab262b3108cbf4bd89f06a980aee07
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-08-25 12:25:29 +0200

    Create def optional value in list cards

--- core/src/main/scala/it/parttimeteam/core/prolog/converter/PrologGameConverter.scala
+++ core/src/main/scala/it/parttimeteam/core/prolog/converter/PrologGameConverter.scala
@@ -1,6 +1,6 @@
 package it.parttimeteam.core.prolog.converter
 
-import alice.tuprolog.{Prolog, Term}
+import alice.tuprolog.{Term}
 import it.parttimeteam.core.cards.Card
 
 /**
@@ -8,20 +8,20 @@ import it.parttimeteam.core.cards.Card
  */
 class PrologGameConverter extends PrologConverter {
 
-  val prolog: Prolog = new Prolog()
-
-  /**
-   * Convert tuple-list in string
-   */
-  val convertList: List[(Int, String)] => String = list => {
-    "([" + list.mkString(",") + "])."
-  }
-
   override def toBoolean(list: List[Term]): Boolean = list.nonEmpty
 
   override def toStringAndReplace(term: Term): String = term.toString.replace("'", "")
 
   override def cardsConvert(cards: List[Card]): String = {
-    convertList(for (card <- cards) yield (card.rank.value, card.suit.name))
+    val tupleCard = for (card <- cards) yield (card.rank.value, card.suit.name)
+    "([" + tupleCard.mkString(",") + "])."
+  }
+
+  def valueCards(cards: List[(Int, String)]): Unit = {
+
+    def _valueCards(cards: List[(Int, String)]) {
+      for (List(first, second) <- cards.grouped(2)) {}
+    }
+    _valueCards(cards)
   }
 }
