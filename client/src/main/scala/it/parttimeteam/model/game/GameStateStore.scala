abd4ade3d214f8f9c2beaa149542bc632fce50d7
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-08-26 18:43:17 +0200

    GameStateStore tests definition

--- client/src/main/scala/it/parttimeteam/model/game/GameStateStore.scala
+++ client/src/main/scala/it/parttimeteam/model/game/GameStateStore.scala
@@ -9,31 +9,57 @@ import it.parttimeteam.gamestate.PlayerGameState
  * Maintains and updates the local game state
  */
 trait GameStateStore {
+
+  def currentState: PlayerGameState
+
   /**
-    * Add the card drown to the player hand
-    *
-    * @param card the card drawn
-    * @return the updated state
-    */
+   * Add the card drown to the player hand
+   *
+   * @param card the card drawn
+   * @return the updated state
+   */
   def onCardDrawn(card: Card): PlayerGameState
 
   /**
-    * Replace the current state with the new one
-    *
-    * @param state the new state
-    * @return the updated state
-    */
+   * Replace the current state with the new one
+   *
+   * @param state the new state
+   * @return the updated state
+   */
   def onStateChanged(state: PlayerGameState): PlayerGameState
 
-  /***
-    * Updates local state after:
-    * * a card combination picked
-    * * a card combination made
-    * * a card combination updated
-    *
-    * @param hand the player hand
-    * @param board the  game board
-    * @return the updated state
-    */
+  /** *
+   * Updates local state after:
+   * * a card combination picked
+   * * a card combination made
+   * * a card combination updated
+   *
+   * @param hand  the player hand
+   * @param board the  game board
+   * @return the updated state
+   */
   def onLocalTurnStateChanged(hand: Hand, board: Board): PlayerGameState
+
 }
+
+object GameStateStore {
+
+  def apply(initialState: PlayerGameState): GameStateStore = new GameStateStoreImpl(initialState)
+
+  /**
+   *
+   * @param currentState the initial state
+   */
+  private class GameStateStoreImpl(var currentState: PlayerGameState) extends GameStateStore {
+
+    override def onCardDrawn(card: Card): PlayerGameState = ???
+
+    override def onStateChanged(state: PlayerGameState): PlayerGameState = ???
+
+    override def onLocalTurnStateChanged(hand: Hand, board: Board): PlayerGameState = ???
+
+  }
+
+}
+
+
