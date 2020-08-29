60f09abc26bd3f7a06c4a78d5c2402c649a2f8bd
lollo_pagno <lorenzo.pagnini@yahoo.it>
2020-08-29 10:32:00 +0100

    Create object for manipolate string

---
+++ core/src/main/scala/it/parttimeteam/core/prolog/converter/PrologUtils.scala
@@ -0,0 +1,26 @@
+package it.parttimeteam.core.prolog.converter
+
+import alice.tuprolog.Term
+import scala.util.matching.Regex
+
+object PrologUtils {
+
+  val pattern: Regex = "[0-9]+,[a-zA-Z]+".r
+
+  def utils(cards: Seq[Term]): List[Regex.Match] = {
+
+    val cardsString: String = replaceString(replaceSeq(cards.filter(term => term.isList), "','"))
+    pattern findAllMatchIn substring(cardsString, 1, cardsString.length-1) toList
+  }
+
+  private def replaceSeq(term: Seq[Term], replace: String): String = term.toString.replace(replace, "")
+
+  private def replaceString(stringToReplace: String): String = {
+    stringToReplace.replace("List", "").replace("[", "")
+      .replace("]", "").replace(" ", "")
+  }
+
+  private def substring(cardsString:String, start: Int, end:Int) : String = {
+    cardsString.substring(start, end)
+  }
+}
