0f661aefb0ea0607939eee4b8c4a3162df118b87
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-09-01 15:58:40 +0200

    new error objects

--- commons/src/main/scala/it/parttimeteam/messages/GameError.scala
+++ commons/src/main/scala/it/parttimeteam/messages/GameError.scala
@@ -7,3 +7,8 @@ case object InvalidUserIdError extends GameError
 case object InvalidPlaysError extends GameError
 
 case class PlayerActionNotValidError(reason: String) extends GameError
+
+case object PlayerLeftGameError extends GameError
+
+case object PrivateLobbyIdNotValidError extends GameError
+
