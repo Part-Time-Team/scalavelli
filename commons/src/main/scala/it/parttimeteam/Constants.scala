6640b4081f764311847540db71b50e164f3e8aa9
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-09-06 17:17:22 +0200

    Fix server port constant.

--- commons/src/main/scala/it/parttimeteam/Constants.scala
+++ commons/src/main/scala/it/parttimeteam/Constants.scala
@@ -10,7 +10,7 @@ object Constants {
     final val SERVER_ACTOR_SYSTEM_NAME = "ScalavelliServer"
     final val SERVER_LOBBY_ACTOR_NAME = "lobby"
     final val SERVER_ADDRESS = "localhost"
-    final val SERVER_PORT = 8081
+    final val SERVER_PORT = 5150
   }
 
 }
