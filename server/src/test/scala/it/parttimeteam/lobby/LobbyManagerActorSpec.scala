2c68fce3450699d89e51be341f993ef59c581b1d
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-09-10 21:18:53 +0200

    improved lobby manager actor test code

--- server/src/test/scala/it/parttimeteam/lobby/LobbyManagerActorSpec.scala
+++ server/src/test/scala/it/parttimeteam/lobby/LobbyManagerActorSpec.scala
@@ -1,7 +1,7 @@
 package it.parttimeteam.lobby
 
 import akka.actor.ActorSystem
-import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit, TestProbe}
 import com.typesafe.config.ConfigFactory
 import it.parttimeteam.messages.LobbyMessages._
 import org.scalatest.BeforeAndAfterAll
@@ -25,69 +25,47 @@ class LobbyManagerActorSpec extends TestKit(ActorSystem("test", ConfigFactory.lo
       client.expectMsgType[Connected]
     }
 
-
     "accept a public lobby connection" in {
       val lobbyActor = system.actorOf(LobbyManagerActor.props())
       val client = TestProbe()
       lobbyActor ! Connect(client.ref)
-      client.expectMsgPF() {
-        case Connected(id) => {
-          lobbyActor ! JoinPublicLobby(id, "user", NUMBER_OF_PLAYERS)
-          client.expectMsgType[UserAddedToLobby]
-        }
-      }
-
-
+      val id = client.expectMsgPF() { case Connected(id) => id }
+      lobbyActor ! JoinPublicLobby(id, "user", NUMBER_OF_PLAYERS)
+      client.expectMsgType[UserAddedToLobby]
     }
 
     "create a new private lobby" in {
       val lobbyActor = system.actorOf(LobbyManagerActor.props())
       val client = TestProbe()
       lobbyActor ! Connect(client.ref)
-      client.expectMsgPF() {
-        case Connected(id) => {
-          lobbyActor ! CreatePrivateLobby(id, "user", NUMBER_OF_PLAYERS)
-          client.expectMsgType[PrivateLobbyCreated]
-        }
-      }
+      val id = client.expectMsgPF() { case Connected(id) => id }
+      lobbyActor ! CreatePrivateLobby(id, "user", NUMBER_OF_PLAYERS)
+      client.expectMsgType[PrivateLobbyCreated]
     }
 
     "accept a private lobby connection request if private lobby exists" in {
       val lobbyActor = system.actorOf(LobbyManagerActor.props())
       val client = TestProbe()
       lobbyActor ! Connect(client.ref)
-      client.expectMsgPF() {
-        case Connected(id) => {
-          lobbyActor ! CreatePrivateLobby(id, "user", NUMBER_OF_PLAYERS)
-          val secondPlayer = TestProbe()
-          client.expectMsgPF() {
-            case PrivateLobbyCreated(lobbyCode) => {
-              lobbyActor ! Connect(secondPlayer.ref)
-              secondPlayer.expectMsgPF() {
-                case Connected(secondId) => {
-                  secondPlayer.send(lobbyActor, JoinPrivateLobby(secondId, "secondPlayer", lobbyCode))
-                  secondPlayer.expectMsgType[UserAddedToLobby]
-                }
-              }
-            }
-          }
-        }
-      }
+      val firstClientId = client.expectMsgPF() { case Connected(id) => id }
+      lobbyActor ! CreatePrivateLobby(firstClientId, "user", NUMBER_OF_PLAYERS)
+      val secondPlayer = TestProbe()
+      val lobbyCode = client.expectMsgPF() { case PrivateLobbyCreated(lobbyCode) => lobbyCode }
+      lobbyActor ! Connect(secondPlayer.ref)
+      val secondClientId = secondPlayer.expectMsgPF() { case Connected(secondId) => secondId }
+      secondPlayer.send(lobbyActor, JoinPrivateLobby(secondClientId, "secondPlayer", lobbyCode))
+      secondPlayer.expectMsgType[UserAddedToLobby]
     }
 
   }
 
   "send an error message when user tries to join to an unexisting private lobby" in {
-    val lobbyActor = system.actorOf(LobbyManagerActor.props())
+    val lobbyActor = TestActorRef[LobbyManagerActor](LobbyManagerActor.props())
     val client = TestProbe()
     lobbyActor ! Connect(client.ref)
-    client.expectMsgPF() {
-      case Connected(id) => {
-        lobbyActor ! JoinPrivateLobby(id, "user", "jfhsjkdfhsjkadl")
-        client.expectMsg(LobbyErrorOccurred(LobbyError.PrivateLobbyIdNotValid))
-      }
-    }
-
+    val id = client.expectMsgPF() { case Connected(id) => id }
+    lobbyActor ! JoinPrivateLobby(id, "user", "jfhsjkdfhsjkadl")
+    client.expectMsg(LobbyErrorOccurred(LobbyError.PrivateLobbyIdNotValid))
   }
 
 }
